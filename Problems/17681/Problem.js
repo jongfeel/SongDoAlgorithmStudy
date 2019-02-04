739e9e044c4bff892b4a8bb81ffcd04a95a7cd05
Kim Jong Feel <DeveloperFEEL@gmail.com>
2019-02-05 00:56:09 +0900

    add sovled code to problem.js, add one more thing description to README.md

--- Problems/17681/Problem.js
+++ Problems/17681/Problem.js
@@ -1,6 +1,22 @@
 function solution(n, arr1, arr2) {
     var answer = [];
+    let bitwiseOR = [];
+    
+    for (let i=0; i<n; i++) {
+        bitwiseOR[i] = arr1[i] | arr2[i];
+        let mappingResult = bitwiseOR[i].toString(2).split('').map(bit => bit == 1 ? "#" : " ");
+        //console.log(mappingResult);
+        // space fill left
+        // ex) n == 6, mappingResult.length == 2 ("##")
+        // space fill left 4 and make length 6 ("    ##")
+        for (let j=mappingResult.length; j<n; j++) {
+            mappingResult.unshift(" ");
+        }
+        answer[i] = mappingResult.join("");
+    }
+    
     return answer;
 }
 
-console.log(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]);
+console.log(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]));
+console.log(solution(6, [46, 33, 33 ,22, 31, 50], [27 ,56, 19, 14, 14, 10]));
