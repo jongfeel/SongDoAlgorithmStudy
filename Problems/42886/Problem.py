d1d8fe566855add18793d40df1749a4167ba06a3
Kim Jong Feel <DeveloperFEEL@gmail.com>
2019-03-02 23:35:37 +0900

    add problem.py comment, why answer variable is 1

--- Problems/42886/Problem.py
+++ Problems/42886/Problem.py
@@ -4,7 +4,7 @@ def solution(weight):
     #print(sorted(weight)[0:7])
     #for item in list(combinations(sorted(weight)[0:7], 6)):
         #print(sum(item))
-    answer = 1
+    answer = 1 # 무게추 들의 합까지를 측정할 수 있고 그다음 무게가 측정할 수 없는 값이므로 1부터 시작
     for w in sorted(weight):
         if answer < w:
             break
